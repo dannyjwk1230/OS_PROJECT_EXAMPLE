5d0d2ea173446285a42ff157afc62e98faf7e758
KDK-DGU <2022112062@dgu.ac.kr>
2026-05-21 20:31:35 +0900

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -16,6 +16,7 @@ static const CommandEntry COMMANDS[] = {
     {"clear", cmd_clear, "clear terminal"},
     {"cp", cmd_cp, "copy files or directories"},
     {"rmdir", cmd_rmdir, "remove empty directories"},
+    {"touch", cmd_touch, "change file timestamps / create empty file"},
 };
 
 int parse_input(char *input, char **argv)
