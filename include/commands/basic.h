739888813dbcaacf6fa9a0a579ed3ff3c21fc8ac
jiwon-kang-dev <rkdwldnjs0531@gmail.com>
2026-05-22 14:26:12 +0900

    Update basic.h

--- include/commands/basic.h
+++ include/commands/basic.h
@@ -9,5 +9,6 @@ MiniOsStatus cmd_mkdir(MiniOsContext *ctx, int argc, char **argv);
 MiniOsStatus cmd_cat(MiniOsContext *ctx, int argc, char **argv);
 MiniOsStatus cmd_pwd(MiniOsContext *ctx, int argc, char **argv);
 MiniOsStatus cmd_echo(MiniOsContext *ctx, int argc, char **argv); 
+MiniOsStatus cmd_search(MiniOsContext *ctx, int argc, char **argv);
 
 #endif
